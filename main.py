224979a15fe80213129a6ea93892ee95747726ff
pragati20-06 <kalepragati622@gmail.com>
2026-03-11 21:36:20 +0530

    Refactor main.py for improved clarity and functionality

--- main.py
+++ main.py
@@ -3,27 +3,26 @@ from apps import sign_up, login, home
 
 st.set_page_config(
     page_title="Stock Market App",
-    page_icon="chart_with_upwards_trend",
+    page_icon="📈",
 )
 
 def main():
-    
     # Initialize session state variables
     if "logged_in" not in st.session_state:
         st.session_state.logged_in = False
     if "current_page" not in st.session_state:
         st.session_state.current_page = "login"  # Default to login page
+    if "email" not in st.session_state:
+        st.session_state.email = ""
     
     # Navigation logic
     if st.session_state.logged_in:
-        home.app()  # Redirect to home page if logged in
+        home.app()  # Show home page if logged in
     else:
         if st.session_state.current_page == "login":
             login.app()
-        if st.session_state.current_page == "sign_up":
+        elif st.session_state.current_page == "sign_up":
             sign_up.app()
 
 if __name__ == "__main__":
     main()
-
-
