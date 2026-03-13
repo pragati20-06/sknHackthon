6de510945b5a9f15e150c1811dab6c06c81a69f8
pragati20-06 <kalepragati622@gmail.com>
2026-03-13 12:21:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,28 +2,24 @@ import streamlit as st
 from apps import sign_up, login, home
 
 st.set_page_config(
-    page_title="Stock Market App",
-    page_icon="chart_with_upwards_trend",
+    page_title="Stock Market App"
 )
 
 def main():
     
-    # Initialize session state variables
     if "logged_in" not in st.session_state:
         st.session_state.logged_in = False
+        
     if "current_page" not in st.session_state:
-        st.session_state.current_page = "login"  # Default to login page
-    
-    # Navigation logic
+        st.session_state.current_page = "login"
+
     if st.session_state.logged_in:
-        home.app()  # Redirect to home page if logged in
+        home.app()
     else:
         if st.session_state.current_page == "login":
             login.app()
-        if st.session_state.current_page == "sign_up":
+        elif st.session_state.current_page == "sign_up":
             sign_up.app()
 
 if __name__ == "__main__":
     main()
-
-
